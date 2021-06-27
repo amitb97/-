1e4fe6f006dd09e7ce013b1be031cd452a9a5b99
amitb97 <82283309+amitb97@users.noreply.github.com>
2021-06-27 08:50:35 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,31 @@
+﻿var Alert = new CustomAlert();
+
+
+function CustomAlert() {
+
+    this.render = function () {
+
+        //Show Modal
+
+        let popUpBox = document.getElementById('popUpBox');
+
+        popUpBox.style.display = "block";
+
+        //Close Modal
+
+
+        document.getElementById('closeModal').innerHTML = '<button onclick="Alert.ok()">X</button>';
+
+    }
+
+
+
+    this.ok = function () {
+
+        document.getElementById('popUpBox').style.display = "none";
+
+        document.getElementById('popUpOverlay').style.display = "none";
+
+    }
+
+}
